dbc84a339497f6fb25b9984115cc1141273f6751
DESKTOP-DL2I1NT\owner <alex@hawryschuk.com>
2025-09-08 15:00:55 +0800

    refactor: CachedWrapper

--- CachedWrapper.ts
+++ CachedWrapper.ts
@@ -1,21 +1,20 @@
 export class CachedWrapper<T extends object> {
-    private cache = new Map<PropertyKey, unknown>();
+    static instances = new WeakMap<Object, CachedWrapper<Object>>();
+    static getInstance<T extends Object>(o: T) { if (!this.instances.has(o)) this.instances.set(o, new CachedWrapper(o)); return this.instances.get(o)! }
+    static proxy<T extends Object>(o: T): T { return this.getInstance(o).proxy as any; }
 
     constructor(private target: T) { }
-
     ClearCache() { this.cache.clear(); }
-
-    get proxy(): T {
-        return new Proxy(this.target, {
-            get: (obj, prop: string | symbol, receiver: any): any => {
-                if (this.cache.has(prop)) {
-                    return this.cache.get(prop);
-                } else {
-                    const value = Reflect.get(obj, prop, receiver);
-                    this.cache.set(prop, value);
-                    return value;
-                }
-            },
-        });
-    }
+    cache = new Map<PropertyKey, unknown>();
+    proxy = new Proxy(this.target, {
+        get: (obj, prop: string | symbol, receiver: any): any => {
+            if (this.cache.has(prop)) {
+                return this.cache.get(prop);
+            } else {
+                const value = Reflect.get(obj, prop, receiver);
+                this.cache.set(prop, value);
+                return value;
+            }
+        },
+    });
 }
